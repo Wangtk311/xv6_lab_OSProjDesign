bfbaf30687addd2e3b0137102fa001fe94c184ec
waerf <1622484392@qq.com>
2024-08-21 05:04:55 -0700

    first

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t lock[NBUCKET];
+
 struct entry {
   int key;
   int value;
@@ -51,8 +53,10 @@ void put(int key, int value)
     // update the existing key.
     e->value = value;
   } else {
+    pthread_mutex_lock(&lock[i]);
     // the new is new.
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]);
   }
 
 }
@@ -101,6 +105,11 @@ get_thread(void *xa)
 int
 main(int argc, char *argv[])
 {
+ // init lock
+  for (int i = 0; i < NBUCKET; i++){
+    pthread_mutex_init(&lock[i], NULL);
+  } 
+
   pthread_t *tha;
   void *value;
   double t1, t0;
